38fce420256d19b936d191282d1b71aca7e0170a
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2017-11-14 01:01:05 +0100

    Version 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject miley-cyrus/lein-template "0.7.1"
+(defproject miley-cyrus/lein-template "0.8.0"
   :description "A very opinionated Clojure project template."
   :url "https://github.com/dryewo/miley-cyrus"
   :license {:name "Apache License"
