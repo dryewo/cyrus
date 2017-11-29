31a0a1848edf57eeddf4ed4b7abe284a8108ffbb
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2017-11-29 13:54:19 +0100

    Version 0.12.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cyrus/lein-template "0.12.0"
+(defproject cyrus/lein-template "0.12.1"
   :description "A very opinionated Clojure project template."
   :url "https://github.com/dryewo/cyrus"
   :license {:name "Apache License"
